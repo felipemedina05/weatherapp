509f4c76056e78a94dc114b95a3ead7c80aa8509
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-09-22 21:53:05 -0500

    conexion a api clima

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [datos, setDatos] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const getDatos = async () => {
+      const res = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=london&appid=7753563720f9768f952766c255e6ce65"
+      );
+      const data = await res.json();
+      console.log(data)
+      setDatos(data);
+    };
+
+    getDatos();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
