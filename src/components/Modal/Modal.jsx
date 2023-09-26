d810469240855ae351b8897116d1e052d8e8ebeb
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-09-26 01:08:25 -0500

    configurando css

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,11 @@
+
+
+// Then add additional custom code here
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Nav from '../Nav/Nav';
+import "./Modal.css";
 
 function Modal({handleclik,handlesubmit}) {
   const [show, setShow] = useState(false);
@@ -10,20 +14,21 @@ function Modal({handleclik,handlesubmit}) {
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch
+    <> 
+      <Button variant="secondary" onClick={handleShow}>
+        Set Location
       </Button>
 
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+      <Offcanvas  show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton >
+          <Offcanvas.Title></Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav handleclik={handleclik} handlesubmit={handlesubmit}></Nav>
         </Offcanvas.Body>
       </Offcanvas>
-    </>
+      
+      </>
   );
 }
 
